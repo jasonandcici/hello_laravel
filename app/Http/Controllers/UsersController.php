ca6fd6773015f42b7d00a43f0874cbda4509af87
冉超 <756620245@qq.com>
2020-12-18 10:05:01 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
 {
@@ -14,6 +15,19 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'name' => 'required|unique:users|max:50',
+                'email' => 'required|email|unique:users|max:255',
+                'password' => 'required|confirmed|min:6'
+            ]);
+        } catch (ValidationException $e) {
+        }
+        return;
     }
 }
